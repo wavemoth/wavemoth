0dc07b10ea8b8845376410284c9cd3f64bccd2be
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-05-16 19:07:57 +0200

    ENH compute HEALPix phi shifts for each ring

--- spherew/healpix.py
+++ spherew/healpix.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+from numpy import pi
 
 def get_ring_thetas(Nside):
     i = np.arange(1, 4 * Nside - 1)
@@ -11,6 +12,16 @@ def get_ring_thetas(Nside):
     z[2 * Nside:] = -z[2 * Nside - 2::-1]
     theta = np.arccos(z)
     return theta
+
+def get_ring_phi0(Nside):
+    counts = get_ring_pixel_counts(Nside)
+    phi = np.zeros(4 * Nside - 1)
+    ring_indices = np.arange(1, 4 * Nside)
+    phi[:Nside - 1] = pi / (4 * ring_indices[:Nside - 1])
+    phi[Nside - 1:3 * Nside] = pi / 4 / Nside
+    phi[Nside:3 * Nside:2] = 0
+    phi[3 * Nside:] = phi[Nside - 2::-1]
+    return phi
 
 def get_ring_pixel_counts(Nside):
     # North polar cap: Start with 4 pixels, add 4 more per ring
--- spherew/test/test_healpix.py
+++ spherew/test/test_healpix.py
@@ -19,9 +19,6 @@ def test_ring_thetas():
     htheta = np.arccos(hz)[::-1]
     yield assert_almost_equal, theta, htheta
 
-    plt.show()
-
-
 def test_ring_pixel_counts():
     counts = get_ring_pixel_counts(Nside)
     yield eq_, 4 * Nside - 1, len(counts)
@@ -30,3 +27,13 @@ def test_ring_pixel_counts():
     yield eq_, 4 * Nside, counts[2 * Nside]
     yield eq_, 12 * Nside**2, counts.sum()
     yield ok_, np.all(counts == counts[::-1])
+
+def test_ring_phi0():
+    counts = get_ring_pixel_counts(Nside)
+    p0_arr = np.r_[0, np.cumsum(counts)[:-1]]
+    from cmb.mapdatautils import array_vec2ang
+    phi_fid = array_vec2ang(
+        healpix.pix2vec_ring(Nside, np.arange(12 * Nside**2)))[:, 1]
+    phi0 = get_ring_phi0(Nside)
+    
+    yield assert_almost_equal, phi0, phi_fid[p0_arr]

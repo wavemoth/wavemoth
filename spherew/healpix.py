750e78d7f7e9aeab8908f6feab97f2680c9252e7
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-05-16 13:53:33 +0200

    ENH Healpix ring pixel counts

--- spherew/healpix.py
+++ spherew/healpix.py
@@ -11,3 +11,16 @@ def get_ring_thetas(Nside):
     z[2 * Nside:] = -z[2 * Nside - 2::-1]
     theta = np.arccos(z)
     return theta
+
+def get_ring_pixel_counts(Nside):
+    # North polar cap: Start with 4 pixels, add 4 more per ring
+    north_polar_cap = np.arange(4, 4 * Nside, 4)
+    assert len(north_polar_cap) == Nside - 1
+    # Middle belt: 4 * Nside in each ring
+    belt = 4 * Nside * np.ones(2 * Nside + 1, dtype=np.int)
+    # South polar cap: Start with 4 * Nside - 4, subtract 4 per ring
+    south_polar_cap = np.arange(4 * Nside - 4, 0, -4)
+    assert len(south_polar_cap) == Nside - 1
+    counts = np.hstack([north_polar_cap, belt, south_polar_cap])
+    return counts
+
--- spherew/test/test_healpix.py
+++ spherew/test/test_healpix.py
@@ -6,10 +6,11 @@ from numpy import pi
 from nose.tools import eq_, ok_
 from numpy.testing import assert_almost_equal
 
-from spherew.healpix import get_ring_thetas
+from ..healpix import *
+
+Nside = 4
 
 def test_ring_thetas():
-    Nside = 16
     theta = get_ring_thetas(Nside)
     yield ok_, theta.shape, (4 * Nside - 1,)
 
@@ -17,3 +18,15 @@ def test_ring_thetas():
     hz = np.unique(hz)
     htheta = np.arccos(hz)[::-1]
     yield assert_almost_equal, theta, htheta
+
+    plt.show()
+
+
+def test_ring_pixel_counts():
+    counts = get_ring_pixel_counts(Nside)
+    yield eq_, 4 * Nside - 1, len(counts)
+    yield eq_, 4, counts[0]
+    yield eq_, 4, counts[-1]
+    yield eq_, 4 * Nside, counts[2 * Nside]
+    yield eq_, 12 * Nside**2, counts.sum()
+    yield ok_, np.all(counts == counts[::-1])

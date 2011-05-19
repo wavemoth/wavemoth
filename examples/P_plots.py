95577eb7176d71a8353f18fbd5d3a37a26dbe411
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-05-19 13:26:20 +0200

    Script for plots of P_lm matrices

---
+++ examples/P_plots.py
@@ -0,0 +1,20 @@
+from __future__ import division
+
+# Stick .. in PYTHONPATH
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname('__file__'), '..'))
+
+
+from spherew import *
+from spherew.healpix import *
+from cmb import as_matrix
+
+lmax = 300
+Nside = 256
+m = 250
+
+nodes = get_ring_thetas(Nside)
+P = compute_normalized_associated_legendre(m, nodes, lmax)
+
+as_matrix(P[:300, :]).plot()

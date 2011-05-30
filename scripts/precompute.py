faec047015d73cc935a8172141ef8b251d842d25
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-05-30 17:01:43 +0200

    Some changes to precomputation script, fixing a critical bug

--- scripts/precompute.py
+++ scripts/precompute.py
@@ -8,6 +8,21 @@ from spherew.healpix import *
 from spherew.benchmark_utils import *
 from spherew import *
 
+class RealignedStream(object):
+    """
+    Redirects to a wrapped stream, but lies about tell()
+    so that the stream starts at the initialization point.
+    """
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.pos0 = wrapped.tell()
+
+    def tell(self):
+        return self.wrapped.tell() - self.pos0
+
+    def write(self, arg):
+        self.wrapped.write(arg)
+
 def compute(stream, mmax, lmax, Nside, min_rows):
     # Start by leaving room in the beginning of the file for writing
     # offsets
@@ -15,18 +30,16 @@ def compute(stream, mmax, lmax, Nside, min_rows):
     header_pos = stream.tell()
     for i in range(4 * (mmax + 1)):
         write_int64(stream, 0)
-    
-    thetas = get_ring_thetas(Nside, positive_only=True)
+    thetas = get_ring_thetas(Nside, positive_only=False) #TODO
     for m in range(0, mmax + 1):
         print 'Precomputing m=%d of %d' % (m, mmax)
         P = compute_normalized_associated_legendre(m, thetas, lmax)
         for odd in (0, 1):
-            P_subset = P[:, odd::2]
+            P_subset = P[:, :] #TODO
             compressed = butterfly_compress(P, min_rows=min_rows)
             start_pos = stream.tell()
-            compressed.write_to_stream(stream)
+            compressed.write_to_stream(RealignedStream(stream))
             end_pos = stream.tell()
-            print '%x %x %x' % (start_pos, end_pos, header_pos + (4 * m + 2 * odd) * 8)
             stream.seek(header_pos + (4 * m + 2 * odd) * 8)
             write_int64(stream, start_pos)
             write_int64(stream, end_pos - start_pos)
@@ -40,8 +53,7 @@ def compute(stream, mmax, lmax, Nside, min_rows):
 ##                    const=sum, default=max,
 ##                    help='sum the integers (default: find the max)')
 
-Nside = 16
+Nside = 128
 lmax = mmax = 2 * Nside
-
 with file('precomputed.dat', 'wb') as f:
     compute(f, mmax, lmax, Nside, min_rows=32)

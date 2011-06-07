0e5d0363d40e59fa31086400cf74de3c63a1c9a7
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-06-07 16:34:42 +0200

    Play with node collision in interpolation.py

--- examples/interpolation.py
+++ examples/interpolation.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.join(os.path.dirname('__file__'), '..'))
 
 import numpy as np
 from matplotlib import pyplot as plt
+np.seterr(all='warn')
 
 N = 20
 y_grid = np.linspace(0, 1, N)
@@ -21,6 +22,10 @@ m = 5
 n = 6
 #f_n = np.asarray([0.2, 0.4, 0.5, -0.3, 1, 1.3])#, 0.8, -.3, .3, .4])
 x_grid = associated_legendre_roots(m + 2 * n, m)
+
+# Make a colliding point
+y_grid[2] = x_grid[0] + 1e-10
+
 
 a_l = np.asarray([0.2, 0.4, 0.5, -0.3, 1, 1.3])
 P_z = compute_normalized_associated_legendre(
@@ -57,15 +62,44 @@ def get_c(l, m):
 l = m + 2 * n - 2
 c = get_c(m + 2 * n - 2, m)
 
-f_ip = fmm1d(x_grid ** 2, P_m_2n_sub_2_at_x_n * rho_n * f_x, y_grid ** 2)
+in_ = P_m_2n_sub_2_at_x_n * rho_n * f_x
+
+# The FMM operation
+x = x_grid[None, :]
+y = y_grid[:, None]
+if 0:
+    K = 1 / (2 * x * (y - x) + (y - x)**2)
+else:
+    K = 1 / (y**2 - x**2)
+#tmp = K[2, 0]; K[2, :] = 0; K[2, 0] = tmp
+#K *= P_m_2n_y[:, None]
+
+f_ip = np.dot(K, P_m_2n_sub_2_at_x_n * rho_n * f_x)
+
+
+#f_ip[2] = P_m_2n_sub_2_at_x_n[0] * rho_n[0] * f_x[0] / (2 * x_grid[0] * (y_grid[2] - x_grid[0]) + (y_grid[2] - x_grid[0])**2)
+
+
+print P_m_2n_y
+print f_ip
+print c
 f_ip *= c * P_m_2n_y
+print f_ip
+
+
+
+print 'Diff problem spot y vs x: %e' % (f_y[2] - f_x[0])
+print 'Diff problem spot ip vs x: %e' % (f_ip[2] - f_x[0])
+print 'Diff problem spot ip vs y: %e' % (f_ip[2] - f_y[2])
+print 'Diff: %e' % np.linalg.norm(f_ip - f_y)
+
 
 plt.clf()
 #plt.plot(y_grid, 5*P_m_2n_y)
+plt.plot(x_grid, f_x, 'ro')
 plt.plot(z_grid, f_z, 'b-')
 plt.plot(y_grid, f_ip, 'gx')
 plt.plot(y_grid, f_y, 'g.')
-plt.plot(x_grid, f_x, 'ro')
 #plt.plot(y_grid, 1 / (y_grid - x_n[3]), 'r-')
 plt.axhline(0, color='black')
 plt.gca().set_ylim((-3, 3))

a64ddf5d8cf933279799e30a433d790b60828b04
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-06-10 11:14:04 +0200

    Forgot to check in compat.h

---
+++ src/compat.h
@@ -0,0 +1,10 @@
+#ifndef _FASTSHT_COMPAT_H_
+#define _FASTSHT_COMPAT_H_
+
+#if defined(_MSC_VER)
+#define SSE_ALIGNED __declspec(align(16))
+#else
+#define SSE_ALIGNED __attribute__((aligned(16)))
+#endif
+
+#endif

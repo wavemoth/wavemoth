6aeafeda9938da29eb8988b58800ecaba9696f73
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-06-10 13:26:00 +0200

    Doh

--- scripts/precompute.py
+++ scripts/precompute.py
@@ -4,7 +4,7 @@ from __future__ import division
 # Stick .. in PYTHONPATH
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname('__file__'), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 import argparse
 from concurrent.futures import ProcessPoolExecutor

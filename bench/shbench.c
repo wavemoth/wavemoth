31a4aa278021f876694b7d9e7618f06df045c3c6
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-06-17 09:03:32 +0200

    shbench multi-threaded benchmarks

--- bench/shbench.c
+++ bench/shbench.c
@@ -20,7 +20,7 @@ C program to benchmark spherical harmonic transforms
 #define Nside 512
 #define lmax 2 * Nside
 #define mmax lmax
-#define PROFILE_TIME 2.0
+#define PROFILE_TIME 3.0
 
 int N_threads;
 
@@ -75,18 +75,19 @@ void finish_memory_benchmark(double dt) {
   free(mem_buf);
 }
 
-void execute_memory_benchmark() {
+void execute_memory_benchmark(int threadnum) {
   /* Simply accumulate the buffer in a double -- we're going to be very
      IO-bound so we can basically ignore the addition put in in order to
      avoid optimizing things out.   */
   int i, j;
   size_t n = N_mem;
-  double acc;
+  double acc = 0;
   double *buf = mem_buf;
-  #pragma omp parallel for num_threads(N_threads) private(i, j) shared(n) reduction(+:acc)
+#pragma omp parallel for schedule(static, 1) private(j) \
+    firstprivate(threadnum) shared(n) reduction(+:acc)
   for (i = 0; i < N_threads; ++i) {
     for (j = 0; j != n; j += 2) {
-      acc += buf[i * n + j];
+      acc += buf[threadnum * n + j];
     }
   }
   if (acc != 0.0) printf("dummy\n");
@@ -95,27 +96,40 @@ void execute_memory_benchmark() {
 /*
 FLOPS benchmark
 */
-size_t N_dgemm = 1400;
-double *A, *B, *Y;
+size_t N_dgemm = 200;
+double **A, **B, **Y;
+
+void execute_dgemm(int threadnum) {
+  dgemm_rrr(A[threadnum], B[threadnum], Y[threadnum],
+            N_dgemm, N_dgemm, N_dgemm, 1.0);
+}
 
 void setup_dgemm() {
-  A = zeros(N_dgemm * N_dgemm);
-  B = zeros(N_dgemm * N_dgemm);
-  Y = zeros(N_dgemm * N_dgemm);
-  execute_dgemm(); /* warmup */
+  int i;
+  A = malloc(sizeof(void*) * N_threads);
+  B = malloc(sizeof(void*) * N_threads);
+  Y = malloc(sizeof(void*) * N_threads);
+  for (i = 0; i != N_threads; ++i) {
+    A[i] = zeros(N_dgemm * N_dgemm);
+    B[i] = zeros(N_dgemm * N_dgemm);
+    Y[i] = zeros(N_dgemm * N_dgemm);
+  }
 }
 
 void finish_dgemm(double dt) {
-  size_t flops = N_dgemm * N_dgemm * N_dgemm * 2;
+  int i;
+  size_t flops = N_threads * N_dgemm * N_dgemm * N_dgemm * 2;
   printf("  Speed: %.3f GFLOPS\n", flops / dt / 1e9);
+  for (i = 0; i != N_threads; ++i) {
+    free(A[i]);
+    free(B[i]);
+    free(Y[i]);
+  }
   free(A);
   free(B);
   free(Y);
 }
 
-void execute_dgemm() {
-  dgemm_rrr(A, B, Y, N_dgemm, N_dgemm, N_dgemm, 1.0);
-}
 
 
 /*
@@ -124,6 +138,10 @@ Butterfly SHT benchmark
 
 double *sht_input, *sht_output, *sht_work;
 fastsht_plan sht_plan;
+
+void execute_sht(int threadnum) {
+  fastsht_execute(sht_plan);
+}
 
 void setup_sht() {
   FILE *fd;
@@ -149,11 +167,6 @@ void setup_sht() {
     fftw_export_wisdom_to_file(fd);
     fclose(fd);
   }
-  execute_sht(); /* warmup */
-}
-
-void execute_sht() {
-  fastsht_execute(sht_plan);
 }
 
 void finish_sht(double dt) {
@@ -170,17 +183,19 @@ void finish_sht(double dt) {
 Main
 */
 
-typedef void (*voidfunc)(void);
 typedef struct {
   char *name;
-  voidfunc setup, execute;
+  void (*setup)(void); 
+  void (*execute)(int threadnum);
   void (*finish)(double dt);
+  int multithreaded;
 } benchmark_t;
 
 benchmark_t benchmarks[] = {
-  {"SHT", setup_sht, execute_sht, finish_sht},
-  {"memread", setup_memory_benchmark, execute_memory_benchmark, finish_memory_benchmark},
-  {"dgemm", setup_dgemm, execute_dgemm, finish_dgemm},
+  {"SHT", setup_sht, execute_sht, finish_sht, 0},
+  {"memread", setup_memory_benchmark, execute_memory_benchmark, finish_memory_benchmark, 1},
+  {"dgemm", setup_dgemm, execute_dgemm, finish_dgemm, 1},
+  {"dgemm-single", setup_dgemm, execute_dgemm, finish_dgemm, 0},
   {NULL, NULL, NULL}
 };
 
@@ -190,23 +205,32 @@ int main(int argc, char *argv[]) {
   fastsht_plan plan;
   double *input, *output, *work;
   double t0, t1, dt;
-  int n;
+  int n, i;
   FILE *fd;
   benchmark_t *pbench;
-
-  N_threads = omp_get_max_threads();
+  
+  omp_set_dynamic(0);
 
   pbench = benchmarks;
   while (pbench->execute != NULL) {
     printf("%s:\n", pbench->name);
+    N_threads = pbench->multithreaded ? omp_get_max_threads() : 1;
     if (pbench->setup != NULL) pbench->setup();
     t0 = walltime();
     n = 0;
+    #pragma omp parallel for num_threads(N_threads) schedule(static, 1)
+    for (i = 0; i < N_threads; ++i) {
+      pbench->execute(i);
+    }
     do {
-      pbench->execute();
-      n++;
+    #pragma omp parallel for num_threads(N_threads) schedule(static, 1)
+      for (i = 0; i < N_threads; ++i) {
+        pbench->execute(i);
+      }
       t1 = walltime();
+      n++;
     } while (t1 - t0 < PROFILE_TIME);
+    
     t1 = walltime();
     printtime("  ", n, t1 - t0);
     if (pbench->finish) pbench->finish((t1 - t0) / n);

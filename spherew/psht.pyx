2e8c4f19003e2b31fd646e11f824c3631232f7de
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-06-20 14:17:56 +0200

    psht: Allow multiple maps in Cython wrapper

--- spherew/psht.pyx
+++ spherew/psht.pyx
@@ -23,6 +23,10 @@ cdef extern from "psht.h":
     ctypedef struct pshtd_cmplx:
         pass
 
+
+    void psht_make_rectangular_alm_info(int lmax, int mmax, int stride,
+                                        psht_alm_info **alm_info)
+
     void psht_make_general_alm_info(int lmax, int nm, int stride, int *mval,
                                     ptrdiff_t *mstart, psht_alm_info **alm_info)
     void psht_destroy_alm_info(psht_alm_info *info)
@@ -56,16 +60,17 @@ cdef class PshtMmajorHealpix:
     cdef psht_alm_info *alm_info
     cdef psht_geom_info *geom_info
     cdef pshtd_joblist *joblist
-    cdef int Nside
+    cdef readonly int Nside, nmaps
     
-    def __cinit__(self, lmax, Nside):
+    def __cinit__(self, lmax, Nside, nmaps):
         # fill in alm_info
         cdef np.ndarray[int, mode='c'] mval = np.arange(lmax + 1, dtype=np.intc)
         # mvstart is the "hypothetical" a_{0m}
         cdef np.ndarray[ptrdiff_t, mode='c'] mstart = \
              lm_to_idx_mmajor(0, mval, lmax).astype(ptrdiff_dtype)
-        
-        psht_make_general_alm_info(lmax, lmax + 1, 1,
+        mstart *= nmaps
+        self.nmaps = nmaps
+        psht_make_general_alm_info(lmax, lmax + 1, nmaps,
                                    <int*>mval.data,
                                    <ptrdiff_t*>mstart.data,
                                    &self.alm_info)
@@ -81,25 +86,32 @@ cdef class PshtMmajorHealpix:
         if self.joblist != NULL:
             pshtd_destroy_joblist(self.joblist)
 
-
     def alm2map(self,
-                np.ndarray[double complex, mode='c'] alm,
-                np.ndarray[double, mode='c'] map=None,
+                np.ndarray[double complex, ndim=2, mode='c'] alm,
+                np.ndarray[double, ndim=2, mode='c'] map=None,
                 int repeat=1):
+        cdef int j
         if map is None:
-            map = np.zeros(12 * self.Nside**2, np.double)
-        pshtd_add_job_alm2map(self.joblist, <pshtd_cmplx*>alm.data,
-                              <double*>map.data, 0)
+            map = np.zeros((alm.shape[1], 12 * self.Nside**2), np.double)
+        if alm.shape[1] != self.nmaps:
+            raise ValueError('Number of maps does not match nmaps passed in constructor')
+        if not (alm.shape[1] == map.shape[0]):
+            raise ValueError('Arrays not conforming')
+        for j in range(map.shape[0]):
+            pshtd_add_job_alm2map(self.joblist,
+                                  <pshtd_cmplx*>alm.data + j,
+                                  <double*>map.data + j * map.shape[1], 0)
         for i in range(repeat):
             pshtd_execute_jobs(self.joblist, self.geom_info, self.alm_info)
         pshtd_clear_joblist(self.joblist)
         return map
 
 def alm2map_mmajor(alm, map=None, Nside=None, repeat=1):
+    nmaps = alm.shape[1]
     lmax = int(np.sqrt(alm.shape[0])) - 1
     if map is not None:
         assert Nside is None
-        Nside = int(np.sqrt(map.shape[0] // 12))
-    return PshtMmajorHealpix(lmax, Nside).alm2map(alm, map, repeat)
+        Nside = int(np.sqrt(map.shape[1] // 12))
+    return PshtMmajorHealpix(lmax, Nside, nmaps).alm2map(alm, map, repeat)
 
         
--- spherew/test/test_psht.py
+++ spherew/test/test_psht.py
@@ -4,14 +4,20 @@ from cmb.maps import pixel_sphere_map
 from matplotlib import pyplot as plt
 
 def test_basic():
+    nmaps = 3
     Nside = 16
     lmax = 2 * Nside
-    T = PshtMmajorHealpix(lmax=32, Nside=16)
-    alm = np.zeros((lmax + 1)**2, dtype=np.complex128)
-    # m
-    alm[lm_to_idx_mmajor(1, 1, lmax)] = 1 + 2j
-    map = np.zeros(12 * Nside**2, dtype=np.double)
+    T = PshtMmajorHealpix(lmax=32, Nside=16, nmaps=nmaps)
+    alm = np.zeros(((lmax + 1)**2, nmaps), dtype=np.complex128)
+    alm[lm_to_idx_mmajor(1, 1, lmax), :] = 1 + 2j
+    alm[:, 0] *= 1
+    alm[:, 1] *= 1
+    alm[:, 2] *= -1
+    map = np.zeros((nmaps, 12 * Nside**2), dtype=np.double)
     T.alm2map(alm, map, repeat=2)
-    #pixel_sphere_map(map).plot()
-    #plt.show()
+    if 0:
+        pixel_sphere_map(map[0, :]).plot()
+        pixel_sphere_map(map[1, :]).plot()
+        pixel_sphere_map(map[2, :]).plot()
+        plt.show()
     

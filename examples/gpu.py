3cc60ec901e09ff2ada9c5e6137a33ef3358cd7f
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-11-02 16:33:28 +0100

    Add repetition of same kernel over multiple blocks.

--- examples/gpu.py
+++ examples/gpu.py
@@ -26,22 +26,24 @@ m = 0
 lmax = 2 * nside
 odd = 0
 nvecs = 2
-
+nblocks = 10000
 
 thetas = healpix.get_ring_thetas(nside, positive_only=True)
 Lambda = compute_normalized_associated_legendre(m, thetas, lmax, epsilon=1e-100)
 Lambda = Lambda[:, odd::2].T
 
-# Mock input vector
-q = np.sin(np.arange(Lambda.shape[1]) * 0.4)
-q = np.vstack([q] * nvecs).T
-q_cl = cl.to_device(queue, q)
-Lambda_0_cl = cl.to_device(queue, Lambda[0, :].copy())
-Lambda_1_cl = cl.to_device(queue, Lambda[1, :].copy())
-x_squared_cl = cl.to_device(queue, np.cos(thetas)**2)
-out_cl = cl.zeros(queue, (Lambda.shape[0], nvecs), dtype=np.double, order='F')
 
-print q_cl.strides
+def hrepeat(x, n):
+    return np.repeat(x[:, None], n, axis=1).copy('F')
+
+# Mock input vector
+q = hrepeat(np.sin(np.arange(Lambda.shape[1]) * 0.4), nvecs * nblocks).reshape(
+    (Lambda.shape[1], nvecs, nblocks), order='F')
+q_cl = cl.to_device(queue, q)
+Lambda_0_cl = cl.to_device(queue, hrepeat(Lambda[0, :], nblocks))
+Lambda_1_cl = cl.to_device(queue, hrepeat(Lambda[1, :], nblocks))
+x_squared_cl = cl.to_device(queue, hrepeat(np.cos(thetas)**2, nblocks))
+out_cl = cl.zeros(queue, (Lambda.shape[0], nvecs, nblocks), dtype=np.double, order='F')
 
 kernel = ClLegendreKernel(ctx, nvecs=nvecs)
 e = kernel.transpose_legendre_transform(queue, m, m + odd, x_squared_cl, 
@@ -49,13 +51,18 @@ e = kernel.transpose_legendre_transform(queue, m, m + odd, x_squared_cl,
 e.wait()
 dt = (e.profile.end - e.profile.start) * 1e-9
 nk, nx = Lambda.shape
-print (nx * nk * 6) / dt / 1e9, 'GUOP'
+matrix_elements = nblocks * nx * nk
+UOP = matrix_elements * (6 + 2 * nvecs)
+print UOP / dt / 1e9, 'GUOP/s'
 
 a = out_cl.get()
-a0 = np.dot(Lambda, q)
+assert np.all(a[:, :, 0:1] == a)
+a = a[:, :, 0]
 
-print np.hstack([a, a0])
+a0 = np.dot(Lambda, q[:, :, 0])
+
+#print np.hstack([a, a0])
 
 print la.norm(a - a0)
 
-plt.clf()
+#plt.clf()
--- wavemoth/cl/legendre_transform.py
+++ wavemoth/cl/legendre_transform.py
@@ -36,19 +36,21 @@ class ClLegendreKernel(object):
 
     def transpose_legendre_transform(self, queue, m, lmin, x_squared, Lambda_0, Lambda_1,
                                      q, out):
-        check_arrays([(x_squared, 1), (Lambda_0, 1), (Lambda_1, 1), (q, 2), (out, 2)])
+        nblocks = q.shape[2]
+        assert nblocks == out.shape[2] == Lambda_0.shape[1] == Lambda_1.shape[1]
+        check_arrays([(x_squared, 2), (Lambda_0, 2), (Lambda_1, 2), (q, 3), (out, 3)])
         if not (q.shape[1] == out.shape[1] == self.nvecs):
             raise ValueError('q and out arrays do not conform to self.nvecs')
         if not (q.strides[0] == out.strides[0] == 8):
             raise ValueError('q and/or out has non-unit stride on axis 0')
         nx = q.shape[0]
         nk = out.shape[0]
-        if not ((nx,) == Lambda_0.shape == Lambda_1.shape == x_squared.shape):
+        if not (nx == Lambda_0.shape[0] == Lambda_1.shape[0] == x_squared.shape[0]):
             raise ValueError('Lambda_0 and/or Lambda_1 and/or x_squared has wrong shape')
         if nx != 2 * 128:
             raise NotImplementedError()
         return self._transpose_legendre_transform(
-            queue, (nx,), (nx,),
+            queue, (nblocks * nx,), (nx,),
             m, lmin, nk, nx, x_squared.data,
             Lambda_0.data, Lambda_1.data, q.data, out.data)
         

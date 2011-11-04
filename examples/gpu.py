da34ef1fb2d694631622d46fce11d0ba1932ec31
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-11-04 15:07:02 +0100

    WIP Implemented unit test case for dot_and_copy

--- examples/gpu.py
+++ examples/gpu.py
@@ -16,10 +16,12 @@ import socket
 for platform in cl.get_platforms():
     if socket.gethostname() == 'dagss-laptop':
         wanted = 'Intel'
-        nblocks = 500
+        nblocks = 5
+        has_warps = False
     else:
         wanted = 'NVIDIA'
         nblocks = 5000
+        has_warps = True
 
     if wanted in platform.name:
         ctx = cl.Context(platform.get_devices())
@@ -33,48 +35,52 @@ nside = 128
 m = 0
 lmax = 2 * nside
 odd = 0
-nvecs = 2
-repeat = 10
+repeat = 1
 
 thetas = healpix.get_ring_thetas(nside, positive_only=True)
 Lambda = compute_normalized_associated_legendre(m, thetas, lmax, epsilon=1e-100)
 Lambda = Lambda[:, odd::2].T
+Lambda = Lambda[:, :Lambda.shape[0]]
 
+for nvecs in [2]:
+    def hrepeat(x, n):
+        return np.repeat(x[:, None], n, axis=1).copy('F')
 
-def hrepeat(x, n):
-    return np.repeat(x[:, None], n, axis=1).copy('F')
+    # Mock input vector
+    q = hrepeat(np.sin(np.arange(Lambda.shape[1]) * 0.4), nvecs * nblocks).reshape(
+        (Lambda.shape[1], nvecs, nblocks), order='F')
+    q_cl = cl.to_device(queue, q)
+    Lambda_0_cl = cl.to_device(queue, hrepeat(Lambda[0, :], nblocks))
+    Lambda_1_cl = cl.to_device(queue, hrepeat(Lambda[1, :], nblocks))
+    x_squared_cl = cl.to_device(queue,
+                                hrepeat(np.cos(thetas)**2, nblocks)[:Lambda.shape[1], :].copy('C'))
+    out_cl = cl.zeros(queue, (Lambda.shape[0], nvecs, nblocks), dtype=np.double, order='F')
 
-# Mock input vector
-q = hrepeat(np.sin(np.arange(Lambda.shape[1]) * 0.4), nvecs * nblocks).reshape(
-    (Lambda.shape[1], nvecs, nblocks), order='F')
-q_cl = cl.to_device(queue, q)
-Lambda_0_cl = cl.to_device(queue, hrepeat(Lambda[0, :], nblocks))
-Lambda_1_cl = cl.to_device(queue, hrepeat(Lambda[1, :], nblocks))
-x_squared_cl = cl.to_device(queue, hrepeat(np.cos(thetas)**2, nblocks))
-out_cl = cl.zeros(queue, (Lambda.shape[0], nvecs, nblocks), dtype=np.double, order='F')
+    times = []
+    for rep in range(repeat):
+        kernel = ClLegendreKernel(ctx, nthreads=Lambda_0_cl.shape[0], nvecs=nvecs,
+                                  has_warps=has_warps)
+        e = kernel.transpose_legendre_transform(queue, m, m + odd,
+                                                x_squared_cl, Lambda_0_cl, Lambda_1_cl,
+                                                q_cl, out_cl)
+        e.wait()
+        times.append((e.profile.end - e.profile.start) * 1e-9)
 
-times = []
-for rep in range(repeat):
-    kernel = ClLegendreKernel(ctx, nvecs=nvecs)
-    e = kernel.transpose_legendre_transform(queue, m, m + odd, x_squared_cl, 
-                                            Lambda_0_cl, Lambda_1_cl, q_cl, out_cl)
-    e.wait()
-    times.append((e.profile.end - e.profile.start) * 1e-9)
+    dt = min(times)
 
-dt = min(times)
-
-nk, nx = Lambda.shape
-matrix_elements = nblocks * nx * nk
-UOP = matrix_elements * (6 + 2 * nvecs)
-print '%.2e +/- %.2e sec = %.2f GUOP/sec' % (dt, np.std(times), UOP / dt / 1e9)
+    nk, nx = Lambda.shape
+    matrix_elements = nblocks * nx * nk
+    UOP = matrix_elements * (6 + 2 * nvecs)
+    print '%.2e +/- %.2e sec = %.2f GUOP/sec' % (dt, np.std(times), UOP / dt / 1e9)
 
 a = out_cl.get()
-assert np.all(a[:, :, 0:1] == a)
+if not np.all(a[:, :, 0:1] == a):
+    print 'NOT ALL j EQUAL!'
 a = a[:, :, 0]
 
 a0 = np.dot(Lambda, q[:, :, 0])
 
-#print np.hstack([a, a0])
+print np.hstack([a, a0])
 
 print la.norm(a - a0)
 
--- wavemoth/cl/legendre_transform.py
+++ wavemoth/cl/legendre_transform.py
@@ -1,7 +1,7 @@
 import os
 import tempita
 import numpy as np
-import pyopencl as cl
+from . import flatpyopencl as cl
 
 from . import core
 
@@ -15,6 +15,36 @@ def check_arrays(args):
             raise ValueError('array has dtype != np.double')
 
 
+# Decorator to use on functions so that any NumPy array passed
+# in is properly transferred to device memory and back. Does
+# not take into account that one does not wish to transfer *all*
+# data both ways...primarily useful for testing. It is assumed
+# that the first two arguments to the function are 'self' and 'queue'.
+#
+# Also, Python 'int' is turned automatically into np.int32.
+def convertargs():
+    def dec(real_func):
+        def repl_func(self, queue, *args, **kw):
+            transferred = []
+            def convert(arg):
+                if isinstance(arg, np.ndarray):
+                    arr_d = cl.to_device(queue, arg)
+                    transferred.append((arg, arr_d))
+                    return arr_d
+                elif isinstance(arg, int):
+                    return np.int32(arg)
+                else:
+                    return arg
+            new_args = [convert(arg) for arg in args]
+            new_kw = dict([(name, convert(arg)) for name, arg in kw.iteritems()])
+            try:
+                return real_func(self, queue, *new_args, **new_kw)
+            finally:
+                for arr, arr_d in transferred:
+                    arr[...] = arr_d.get()
+        return repl_func
+    return dec
+
 class ClLegendreKernel(object):
     """
 
@@ -22,23 +52,27 @@ class ClLegendreKernel(object):
         each round through global memory
     
     """
-    def __init__(self, ctx, nvecs, **args):
+    def __init__(self, ctx, nvecs, nthreads, **args):
+        self.nthreads = nthreads
+        self.nvecs = nvecs
+
         code = core.instantiate_template('legendre_transform.cl.in',
                                          nvecs=nvecs,
-                                         local_size=2 * 128,
+                                         local_size=nthreads,
+                                         warp_size=32,
                                          **args)
-        self.nvecs = nvecs
         self.prg = cl.Program(ctx, code).build()
         self._transpose_legendre_transform = self.prg.transpose_legendre_transform
         self._transpose_legendre_transform.set_scalar_arg_dtypes(
             [np.int32, np.int32, np.int32, np.int32,
              None, None, None, None, None])
 
-    def transpose_legendre_transform(self, queue, m, lmin, x_squared, Lambda_0, Lambda_1,
-                                     q, out):
+    def transpose_legendre_transform(self, queue, m, lmin,
+                                     x_squared, Lambda_0, Lambda_1, q, out):
         nblocks = q.shape[2]
         assert nblocks == out.shape[2] == Lambda_0.shape[1] == Lambda_1.shape[1]
         check_arrays([(x_squared, 2), (Lambda_0, 2), (Lambda_1, 2), (q, 3), (out, 3)])
+        self.nvecs = q.shape[1]
         if not (q.shape[1] == out.shape[1] == self.nvecs):
             raise ValueError('q and out arrays do not conform to self.nvecs')
         if not (q.strides[0] == out.strides[0] == 8):
@@ -47,10 +81,16 @@ class ClLegendreKernel(object):
         nk = out.shape[0]
         if not (nx == Lambda_0.shape[0] == Lambda_1.shape[0] == x_squared.shape[0]):
             raise ValueError('Lambda_0 and/or Lambda_1 and/or x_squared has wrong shape')
-        if nx != 2 * 128:
-            raise NotImplementedError()
+
+        assert self.nx == self.nk == self.nthreads
+
         return self._transpose_legendre_transform(
-            queue, (nblocks * nx,), (nx,),
+            queue, (nblocks * self.nthreads,), (self.nthreads,),
             m, lmin, nk, nx, x_squared.data,
             Lambda_0.data, Lambda_1.data, q.data, out.data)
-        
+
+    @convertargs()
+    def dot_and_copy(self, queue, P, q, P_local, work_sum):
+        self.prg.dot_and_copy_kernel(queue, (self.nthreads,), (self.nthreads,),
+                                     P.data, q.data, P_local.data, work_sum.data,
+                                     np.int32(P.shape[0]))
